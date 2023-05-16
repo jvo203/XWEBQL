2134597b20d086f650e15383849ea509d017b8b8
jvo203 <chris.zapart@nao.ac.jp>
2023-05-16 17:02:24 +0900

    modified:   Julia/test_sxs.jl

--- Julia/test_sxs.jl
+++ Julia/test_sxs.jl
@@ -1,2 +1,15 @@
 using FITSIO
 
+sxs = homedir() * "/NAO/JAXA/ah100040060sxs_p0px1010_cl.evt"
+f = FITS(sxs)
+
+for hdu in f
+    println(typeof(hdu))
+end
+
+@time x = read(f[2], "X")
+@time y = read(f[2], "Y")
+@time energy = read(f[2], "UPI")
+
+nevents = length(x)
+println("nevents = ", nevents)
