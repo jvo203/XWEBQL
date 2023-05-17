9661a0dbb6eb3eaf5a753d3ac4c2163ff523c52d
jvo203 <chris.zapart@nao.ac.jp>
2023-05-17 16:47:59 +0900

    modified:   Julia/test_sxs_mmap.jl

--- Julia/test_sxs_mmap.jl
+++ Julia/test_sxs_mmap.jl
@@ -1,1 +1,11 @@
-sxs = homedir() * "/NAO/JAXA/ah100040060sxs_p0px1010_cl.evt"
+using Mmap
+
+filename = homedir() * "/NAO/JAXA/ah100040060sxs_p0px1010_cl.evt"
+io = open(filename, "r+")
+sxs = Mmap.mmap(io, Vector{UInt8})
+
+println(typeof(sxs))
+println(length(sxs))
+
+# convert Vector{UInt8} to String
+println(String(sxs[1:2880]))
