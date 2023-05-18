51a4dc33c9915578e9c663888350c211858d448e
jvo203 <chris.zapart@nao.ac.jp>
2023-05-18 16:44:05 +0900

    modified:   C/test_sxs.c

--- C/test_sxs.c
+++ C/test_sxs.c
@@ -7,5 +7,7 @@ int main()
     const char *event_filename = "../../../NAO/JAXA/ah100040060sxs_p0px1010_cl.evt";
     printf("event_filename = %s\n", event_filename);
 
+    // open the event file and mmap it
+
     return 0;
 }
