047422a66e76fb88bee339bc8661e979ba67602e
jvo203 <chris.zapart@nao.ac.jp>
2023-05-18 16:40:23 +0900

    modified:   C/test_sxs.c

--- C/test_sxs.c
+++ C/test_sxs.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+
 #include "sxs.h"
+
+int main()
+{
+    const char *event_filename = "../../../NAO/JAXA/ah100040060sxs_p0px1010_cl.evt";
+    printf("event_filename = %s\n", event_filename);
+
+    return 0;
+}
