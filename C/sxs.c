fbf1cec81655b0a9d3499e721baf10fa22e037b2
jvo203 <chris.zapart@nao.ac.jp>
2023-05-19 12:32:37 +0900

    modified:   C/sxs.c

--- C/sxs.c
+++ C/sxs.c
@@ -48,6 +48,17 @@ bool scan_table_header(const char *sxs, int *naxis1, int *naxis2, int *tfields)
 
         if (strncmp(line, "TFIELDS = ", 10) == 0)
             *tfields = hdr_get_int_value(line + 10);
+
+        // detect the TTYPEXX and TFORMXX lines
+        if (strncmp(line, "TTYPE", 5) == 0)
+        {
+            printf("TTYPE = %.70s\n", line + 10);
+        }
+
+        if (strncmp(line, "TFORM", 5) == 0)
+        {
+            printf("TFORM = %.8s\n", line + 10);
+        }
     }
 
     return false;
