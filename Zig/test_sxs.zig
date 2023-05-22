ae42a10cf9cc8f835fad9ac0e0b09712239f82a5
jvo203 <chris.zapart@nao.ac.jp>
2023-05-22 09:30:13 +0900

    modified:   Zig/test_sxs.zig

--- Zig/test_sxs.zig
+++ Zig/test_sxs.zig
@@ -2,4 +2,11 @@ const std = @import("std");
 
 pub fn main() void {
     std.debug.print("Testing Reading X-Ray SXS event files\n", .{});
+
+    const start = std.time.nanoTimestamp();
+
+    var duration: f64 = @intToFloat(f64, std.time.nanoTimestamp() - start);
+    duration /= 1_000_000;
+
+    std.debug.print("Zig elapsed time: {d:.6} ms\n", .{duration});
 }
