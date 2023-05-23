40ad2a300beca6bee0861b935b71d2d1aacd5b6d
jvo203 <chris.zapart@nao.ac.jp>
2023-05-24 08:53:36 +0900

    modified:   Zig/test_sxs.zig

--- Zig/test_sxs.zig
+++ Zig/test_sxs.zig
@@ -15,66 +15,6 @@ const XEvents = struct {
     columns: []i32,
 };
 
-// create a read_sxs_events function that takes a filename and returns a tuple with x,y,energy arrays
-//fn read_sxs_events(filename: []const u8) (x: []f32, y: []f32, energy: []f32) {
-//   const std = @import("std");
-//  const io = std.io;
-// const mem = std.mem;
-// const os = std.os;
-// const path = std.path;
-// const strconv = std.strconv;
-// const testing = std.testing;
-
-//var file = try io.readFile(filename);
-//defer file.deinit();
-
-//var parser = try strconv.utf8.parseFile(file);
-//defer parser.deinit();
-
-//    var x: []f32 = undefined;
-//   var y: []f32 = undefined;
-//  var energy: []f32 = undefined;
-
-//    while (parser.next()) |line| {
-//       var line = line;
-//      if (line.len == 0) {
-//         continue;
-//    }
-//   if (line[0] == '#') {
-//      continue;
-// }
-// var x_str = try line.split(" ")[0];
-// var y_str = try line.split(" ")[1];
-// var energy_str = try line.split(" ")[2];
-
-//var x_f32 = try strconv.parseFloat(f32, x_str);
-//var y_f32 = try strconv.parseFloat(f32, y_str);
-//var energy_f32 = try strconv.parseFloat(f32, energy_str);
-
-//if (x == undefined) {
-//   x = try mem.slice(f32, parser.allocator, 0, 1);
-//} else {
-//   x = try mem.slice(f32, parser.allocator, x, x.len + 1);
-// }
-//if (y == undefined) {
-//   y = try mem.slice(f32, parser.allocator, 0, 1);
-//} else {
-//   y = try mem.slice(f32, parser.allocator, y, y.len + 1);
-// }
-//if (energy == undefined) {
-//   energy = try mem.slice(f32, parser.allocator, 0, 1);
-//} else {
-//   energy = try mem.slice(f32, parser.allocator, energy, energy.len + 1);
-//}
-
-//x[x.len - 1] = x_f32;
-//y[y.len - 1] = y_f32;
-//energy[energy.len - 1] = energy_f32;
-//}
-
-//return (x, y, energy);
-//}
-
 fn hdr_get_int_value(line: []const u8) !i32 {
     const str = std.mem.trim(u8, line[10..FITS_LINE_LENGTH], " \r\n\t");
     return try std.fmt.parseInt(i32, str, 10);
@@ -246,7 +186,7 @@ fn get_column_offset(columns: []i32, index: i32) usize {
     return @intCast(usize, offset);
 }
 
-fn read_sxs_events(filename: []const u8, allocator: Allocator) !usize {
+fn read_sxs_events(filename: []const u8, allocator: Allocator) !XEvents {
 
     // open the file, get a file descriptor
     const fd = try std.os.open(filename, std.c.O.RDONLY, 0);
@@ -349,7 +289,7 @@ fn read_sxs_events(filename: []const u8, allocator: Allocator) !usize {
         offset += events.NAXIS1;
     }
 
-    return events.NAXIS2;
+    return events;
 }
 
 pub fn main() !void {
@@ -362,9 +302,12 @@ pub fn main() !void {
     const allocator = arena.allocator();
 
     const start = std.time.nanoTimestamp();
-    const num_events = try read_sxs_events(event_filename, allocator);
+    const events = try read_sxs_events(event_filename, allocator);
     var duration: f64 = @intToFloat(f64, std.time.nanoTimestamp() - start);
     duration /= 1_000_000;
 
-    std.debug.print("num_events = {d}, zig elapsed time: {d:.6} ms\n", .{ num_events, duration });
+    std.debug.print("Zig elapsed time: {d:.6} ms\n", .{duration});
+
+    const num_events = events.NAXIS2;
+    std.debug.print("num_events = {d}\n", .{num_events});
 }
