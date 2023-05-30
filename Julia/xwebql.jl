0824dd7e06f5ef623aa15f7be6d7b42c93252a99
jvo203 <chris.zapart@nao.ac.jp>
2023-05-30 18:00:16 +0900

    new file:   htdocs/index.html

--- Julia/xwebql.jl
+++ Julia/xwebql.jl
@@ -1,8 +1,9 @@
 using ArgParse
 using Distributed
 using HTTP
+using JSON
+using Sockets
 using WebSockets
-
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -22,6 +23,8 @@ end
 
 # parse command-line arguments (a config file, port numbers etc.)
 parsed_args = parse_commandline()
+
+TIMEOUT = 60 # [s]
 
 const VERSION_MAJOR = 1
 const VERSION_MINOR = 0
@@ -50,3 +53,276 @@ include("xevent.jl")
 # a global list of FITS objects
 XOBJECTS = Dict{String,XDataSet}()
 XLOCK = ReentrantLock()
+
+function serveFile(path::String)
+    # strip out a question mark (if there is any)
+    pos = findlast("?", path)
+
+    if !isnothing(pos)
+        path = SubString(path, 1:(pos[1]-1))
+    end
+
+    # cache a response
+    headers = ["Cache-Control" => "public, max-age=86400"]
+
+    # add mime types
+    if endswith(path, ".htm") || endswith(path, ".html")
+        push!(headers, "Content-Type" => "text/html")
+    end
+
+    if endswith(path, ".txt")
+        push!(headers, "Content-Type" => "text/plain")
+    end
+
+    if endswith(path, ".css")
+        push!(headers, "Content-Type" => "text/css")
+    end
+
+    if endswith(path, ".js")
+        push!(headers, "Content-Type" => "application/javascript")
+    end
+
+    if endswith(path, ".wasm")
+        push!(headers, "Content-Type" => "application/wasm")
+    end
+
+    if endswith(path, ".pdf")
+        push!(headers, "Content-Type" => "application/pdf")
+    end
+
+    if endswith(path, ".ico")
+        push!(headers, "Content-Type" => "image/x-icon")
+    end
+
+    if endswith(path, ".png")
+        push!(headers, "Content-Type" => "image/png")
+    end
+
+    if endswith(path, ".gif")
+        push!(headers, "Content-Type" => "image/gif")
+    end
+
+    if endswith(path, ".webp")
+        push!(headers, "Content-Type" => "image/webp")
+    end
+
+    if endswith(path, ".svg")
+        push!(headers, "Content-Type" => "image/svg+xml")
+    end
+
+    if endswith(path, ".jpeg") || endswith(path, ".jpg")
+        push!(headers, "Content-Type" => "image/jpeg")
+    end
+
+    if endswith(path, ".mp4")
+        push!(headers, "Content-Type" => "video/mp4")
+    end
+
+    try
+        return isfile(path) ? HTTP.Response(200, headers; body=read(path)) :
+               HTTP.Response(404, "$path Not Found.")
+    catch e
+        return HTTP.Response(404, "Error: $e")
+    end
+end
+
+function serveDirectory(request::HTTP.Request)
+    headers = ["Content-Type" => "application/json"]
+
+    params = HTTP.queryparams(HTTP.URI(request.target))
+
+    dir = ""
+
+    try
+        dir = params["dir"]
+
+        # on Windows remove the root slash
+        if Sys.iswindows() && length(dir) > 0
+            dir = lstrip(dir, '/')
+        end
+
+        if dir == ""
+            dir = homedir()
+        end
+    catch _
+        # if they keyword is not found fall back on a home directory
+        dir = homedir()
+    end
+
+    # append a slash so that on Windows "C:" becomes "C:/"
+    if dir == "C:"
+        dir = dir * "/"
+    end
+
+    println("Scanning $dir ...")
+
+    resp = chop(JSON.json(Dict("location" => dir)), tail=1) * ", \"contents\":["
+
+    elements = false
+
+    try
+        foreach(readdir(dir)) do f
+            filename = lowercase(f)
+
+            if !startswith(filename, ".")
+
+                path = dir * Base.Filesystem.path_separator * f
+
+                info = stat(path)
+
+                if isdir(path)
+                    dict = Dict(
+                        "type" => "dir",
+                        "name" => f,
+                        "last_modified" => Libc.strftime(info.mtime),
+                    )
+
+                    resp *= JSON.json(dict) * ","
+                    elements = true
+                end
+
+                if isfile(path)
+
+                    # filter the filenames
+                    if endswith(filename, ".evt") || endswith(filename, ".evt.gz")
+
+                        dict = Dict(
+                            "type" => "file",
+                            "size" => info.size,
+                            "name" => f,
+                            "last_modified" => Libc.strftime(info.mtime),
+                        )
+
+                        resp *= JSON.json(dict) * ","
+                        elements = true
+                    end
+                end
+
+            end
+        end
+    catch _
+    end
+
+    if elements
+        resp = chop(resp, tail=1) * "]}"
+    else
+        resp *= "]}"
+    end
+
+    try
+        return HTTP.Response(200, headers; body=resp)
+    catch e
+        return HTTP.Response(404, "Error: $e")
+    end
+end
+
+function exitFunc(exception=false)
+    global ws_server
+
+    try
+        println("WebSocket Server .out channel: ", string(take!(ws_server.out)))
+        close(ws_server)
+    catch e
+        println(e)
+    end
+
+    @info "XWEBQL shutdown."
+    exit()
+end
+
+if Base.isinteractive()
+    Base.exit_on_sigint(false)
+else
+    Base.atexit(exitFunc)
+end
+
+function gracefullyShutdown(request::HTTP.Request)
+    @async exitFunc(true)
+
+    return HTTP.Response(200, "Shutting down $(SERVER_STRING)")
+end
+
+
+function serveROOT(request::HTTP.Request)
+    # @show request
+    # @show request.method
+    # @show HTTP.header(request, "Content-Type")
+    # @show HTTP.payload(request)
+    @show request.target
+
+    # prevent a simple directory traversal
+    if occursin("../", request.target) || occursin("..\\", request.target)
+        return HTTP.Response(404, "Not Found")
+    end
+
+    path = HT_DOCS * HTTP.unescapeuri(request.target)
+
+    if request.target == "/"
+        path *= "index.html"
+    end
+
+    return serveFile(path)
+end
+
+const XROUTER = HTTP.Router()
+HTTP.register!(XROUTER, "GET", "/", serveROOT)
+HTTP.register!(XROUTER, "GET", "/exit", gracefullyShutdown)
+HTTP.register!(XROUTER, "GET", "/get_directory", serveDirectory)
+
+println("$SERVER_STRING")
+println("DATASET TIMEOUT: $(TIMEOUT)s")
+println("Point your browser to http://localhost:$HTTP_PORT")
+println(
+    "Press CTRL+C or send SIGINT to exit. Alternatively point your browser to http://localhost:$HTTP_PORT/exit",
+)
+
+# Sockets.localhost or Sockets.IPv4(0)
+host = Sockets.IPv4(0)
+
+function ws_gatekeeper(req, ws)
+    orig = WebSockets.origin(req)
+    target = HTTP.unescapeuri(req.target)
+
+    @info "\nOrigin: $orig   Target: $target   subprotocol: $(subprotocol(req))"
+
+    # check if there is a '<sessionid>/<datasetid>' present in <target>
+    pos = findlast("/", target)
+
+    if !isnothing(pos)
+        sessionid = SubString(target, pos[1] + 1)
+        @info "\n[ws] sessionid $sessionid"
+
+        target = SubString(target, 1, pos[1] - 1)
+        pos = findlast("/", target)
+
+        if !isnothing(pos)
+
+            targets = SubString(target, pos[1] + 1)
+            ids = split(targets, ";")
+
+            @info "\n[ws] datasetid $(ids[1])"
+
+            # ws_coroutine(ws, ids)
+        else
+            @info "[ws] Missing datasetid"
+        end
+    else
+        @info "[ws] Missing sessionid"
+    end
+
+end
+
+ws_handle(req) = SERVER_STRING |> WebSockets.Response
+const ws_server = WebSockets.ServerWS(ws_handle, ws_gatekeeper)
+
+@async WebSockets.serve(ws_server, host, WS_PORT)
+
+try
+    HTTP.serve(XROUTER, host, UInt16(HTTP_PORT))
+catch e
+    println(e)
+    warn(e)
+    typeof(e) == InterruptException && rethrow(e)
+finally
+    exitFunc()
+end
