766c74697c938149615b1a514260e4eac228fba7
jvo203 <chris.zapart@nao.ac.jp>
2023-06-26 15:00:41 +0900

    new file:   Julia/ZfpCompression.jl

---
+++ Julia/ZfpCompression.jl
@@ -0,0 +1,9 @@
+module ZfpCompression
+
+using zfp_jll
+
+export zfp_compress, zfp_decompress, zfp_decompress!
+
+include("zfp.jl")
+
+end
--- Julia/xwebql.jl
+++ Julia/xwebql.jl
@@ -5,7 +5,9 @@ using HTTP
 using JSON
 using Sockets
 using WebSockets
-using ZfpCompression
+#using ZfpCompression
+
+include("ZfpCompression.jl")
 
 function parse_commandline()
     s = ArgParseSettings()
@@ -814,7 +816,7 @@ function streamImageSpectrum(http::HTTP.Streams.Stream)
         end
 
         println("typeof(pixels) = ", typeof(pixels))
-        compressed_pixels = zfp_compress(pixels, precision=prec)
+        compressed_pixels = ZfpCompression.zfp_compress(pixels, precision=prec)
         write(http, Int32(length(compressed_pixels)))
         write(http, compressed_pixels)
 
@@ -844,7 +846,7 @@ function streamImageSpectrum(http::HTTP.Streams.Stream)
 
             # spectrum
             println("typeof(spectrum) = ", typeof(spectrum))
-            compressed_spectrum = zfp_compress(
+            compressed_spectrum = ZfpCompression.zfp_compress(
                 spectrum,
                 precision=SPECTRUM_HIGH_PRECISION,
             )
