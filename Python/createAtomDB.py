04d11858f026ba3f412298afc0f38886189c5c9e
jvo203 <chris.zapart@nao.ac.jp>
2023-07-11 11:22:06 +0900

    modified:   Python/createAtomDB.py

--- Python/createAtomDB.py
+++ Python/createAtomDB.py
@@ -2,4 +2,21 @@ import sqlite3
 import hashlib
 import urllib
 import time
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
+
+conn = sqlite3.connect("atom.db")
+c = conn.cursor()
+
+strSQL = "DROP TABLE IF EXISTS lines ;"
+
+try:
+    c.execute(strSQL)
+except sqlite3.OperationalError as err:
+    print(err, ":", strSQL)
+
+strSQL = "CREATE TABLE lines (species TEXT, name TEXT, frequency REAL, qn TEXT, cdms_intensity REAL, lovas_intensity REAL, E_L REAL, linelist TEXT, hash TEXT UNIQUE) ;"
+
+try:
+    c.execute(strSQL)
+except sqlite3.OperationalError as err:
+    print(err, ":", strSQL)
