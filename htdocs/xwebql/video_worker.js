7fdd79d1a8b28e2e3717fa546bcfe38c53976f2f
jvo203 <chris.zapart@nao.ac.jp>
2024-09-05 09:09:36 +0900

    new file:   htdocs/xwebql/video_worker.js

---
+++ htdocs/xwebql/video_worker.js
@@ -0,0 +1,10 @@
+console.log('WebCodecs API Video Worker initiate');
+self.addEventListener('message', function (e) {
+/*    importScripts('marchingsquares-isobands.min.js');
+    importScripts('marchingsquares-isocontours.min.js');
+    console.log('[CRWORKER]: level = ' + e.data.level);
+    var band = MarchingSquaresJS.isoBands(e.data.data, e.data.lowerBand, e.data.upperBand - e.data.lowerBand);
+    self.postMessage(band);
+    self.close();*/
+}, false);
+
