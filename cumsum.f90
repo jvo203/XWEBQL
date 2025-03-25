37c2f421a26fac2ca6a455d67ce6d1592f00edc8
jvo203 <chris.zapart@nao.ac.jp>
2025-03-25 17:06:49 +0900

    improved cumsum in Fortran

--- cumsum.f90
+++ cumsum.f90
@@ -4,3 +4,13 @@ real(dp) :: r(size(a))
 integer :: i
 r(:) = [(sum(a(1:i)),i=1,size(a))]
 end function cumsum
+
+subroutine cumsum(a,b)
+real(kind=r8) :: a(:),b(size(a))
+integer(kind=i8) :: i
+b(1)=a(1)
+do i = 2, size(a)
+    b(i) = b(i-1) + a(i)
+enddo
+return
+end subroutine cumsum
