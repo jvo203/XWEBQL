fec7cbc97d844f9e35d208ab987edf3fcf5f0249
jvo203 <chris.zapart@nao.ac.jp>
2025-03-26 09:56:24 +0900

    implemented an in-place cumsum

--- test_bl.f90
+++ test_bl.f90
@@ -3,11 +3,32 @@ program test
    use omp_lib
    implicit none
 
+   call test_cumsum()
+
 contains
-   subroutine test_bl()
-      real(kind=c_float) :: a(10),b(10)
-      integer :: i
+   subroutine test_cumsum()
+      real(kind=c_float) :: a(10)
+
       a = [1,2,3,4,5,6,7,8,9,10]
 
-   end subroutine test_bl
+      print *, "Original array:"
+      print *, a
+
+      print *, "Cumulative sum:"
+      call cumsum(a)
+      print *, a
+
+   end subroutine test_cumsum
+
+
+   ! in-place cumulative sum
+   subroutine cumsum(x)
+      real(kind=c_float), intent(inout) :: x(:)
+      integer(kind=8) :: i
+
+      do i = 2, size(x)
+         x(i) = x(i-1) + x(i)
+      end do
+
+   end subroutine cumsum
 end program test
