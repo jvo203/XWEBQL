7f876869917f853e4e92d6d08f43f637e450b80f
jvo203 <chris.zapart@nao.ac.jp>
2025-03-27 17:13:03 +0900

    modified:   fbh.f90

--- fbh.f90
+++ fbh.f90
@@ -2,6 +2,11 @@ module fbh
    use iso_c_binding
    implicit none
 
+
+   type, bind(c) :: BayesHistogram
+      type(c_ptr) :: centers, widths, heights
+      integer(kind=c_int) :: n
+   end type BayesHistogram
 contains
    real(kind=c_float) function fast_bayesian_histogram(energy, n) bind(c)
       integer(kind=c_int), intent(in) :: n
