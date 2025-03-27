56151c49e14e4b24a4f1f252dc86597d2d211e6d
jvo203 <chris.zapart@nao.ac.jp>
2025-03-27 09:50:23 +0900

    created sample energy data

---
+++ Julia/BayesHistogram_debug.jl
@@ -0,0 +1,169 @@
+"""BayesHistogram.jl
+    main procedure: 
+    function bayesian_blocks(
+        t::AbstractVector{T};
+        weights::AbstractVector{W} = one.(t),        
+        prior = BIC(),
+        resolution = Inf,
+        min_counts::Real = 0,
+    ) where {T<:Real,W<:Real}
+"""
+module BayesHistogram
+
+include("SimultaneousSortperm.jl")
+using .SimultaneousSortperm
+
+struct BHist{T<:Real}
+    edges::Vector{T}
+    counts::Vector{T}
+    centers::Vector{T}
+    widths::Vector{T}
+    heights::Vector{T}
+end
+
+include("priors.jl")
+include("utils.jl")
+
+function sort_sane(raw_x, raw_w)
+    p = ssortperm(raw_x)
+    tmp_x = raw_x[p]
+    tmp_w = raw_w[p]
+    f = tmp_w .> 0
+
+    tmp_x[f], tmp_w[f]
+end
+
+function sanitize(raw_x, raw_w)
+    # first round:
+    # - sort by x
+    # - skip values with weights < 0
+    x, w = sort_sane(raw_x, raw_w)
+    # second round:
+    # - merge entries with same x value
+    m = fill(true, length(x))
+    for i = 2:length(x)
+        if x[i] == x[i-1] && m[i-1] && m[i] # merge
+            m[i-1] = false
+            w[i] += w[i-1]
+            w[i-1] = 0
+        end
+    end
+    return x[m], w[m]
+end
+
+function count_between_edges(edges::AbstractVector{T}, weights::AbstractVector{T}, observations::AbstractVector{T}, shift::Bool=false) where {T<:Real}
+    i = 1
+    # take the underlying type of the edges    
+    out = zeros(T, length(edges) - 1 + shift)
+    for (el, w) in zip(observations, weights)
+        while !(edges[i] <= el <= edges[i+1])
+            i += 1
+        end
+        out[i+shift] += w
+    end
+    return out
+end
+
+function build_blocks(t, edges, weights)
+    centers = @views(edges[begin:end-1] .+ edges[begin+1:end]) ./ 2
+    counts = count_between_edges(edges, weights, t)
+    total = sum(counts)
+    widths = diff(edges)
+    heights = counts ./ (total .* widths)
+    return BHist(edges, counts, centers, widths, heights)
+end
+
+"""
+    function bayesian_blocks(
+        datas::AbstractVector{T};
+        weights::AbstractVector{W} = one.(t),        
+        prior = BIC(),
+        resolution = Inf,
+        min_counts::Real = 0,
+    ) where {T<:Real,W<:Real}
+
+- `datas`: Observations
+- `weights`: sample weight of each observation
+- `prior`: choose from `NoPrior`, `Pearson`, `Geometric`, `BIC`, `AIC`, `HQIC`, `FPR`, `Scargle` (identical to `FPR`)
+- resolution: handles on how fine we count along the `datas axis
+- min_counts: minimum sum of weights of a block that can be splitted.
+
+"""
+function bayesian_blocks(
+    t::AbstractVector{T};
+    weights::AbstractVector{W}=one.(t),
+    prior=BIC(),
+    resolution=Inf,
+    min_counts::Real=0,
+) where {T<:Real,W<:Real}
+    # copy and sort the arrays
+    t, weights = sanitize(t, weights)
+
+    # check trivial cases
+    N = length(t)
+    if N == 0
+        return build_blocks(T[], T[-Inf, Inf], T[])
+    elseif N == 1
+        return build_blocks(t, T[t[1], t[1]], weights)
+    end
+    # create cell edges
+    edges = [t[begin]; @views(t[begin+1:end] .+ t[begin:end-1]) ./ 2; t[end]]
+
+    # make cumulative weights
+    wh_in_edge = count_between_edges(edges, weights, t, true)
+    wh_in_edge .= cumsum(wh_in_edge)
+
+    # arrays needed for the iteration
+    best = zeros(T, N)
+    lasts = zeros(Int, N)
+
+    extent = t[end] - t[begin]
+    # by default dt is 0 because resolution is Inf
+    dt = max(abs(extent / resolution), zero(T))
+    # Start with first data cell; add one cell at each iteration
+    @inbounds for Q = 1:N
+        fit_max = -Inf
+        i_max = 0
+        for i = 1:Q
+            cnt_in_range = wh_in_edge[Q+1] - wh_in_edge[i]
+            cnt_in_range < min_counts && break
+            width = edges[Q+1] - edges[i]
+            width <= dt && break
+
+            fitness =
+                cnt_in_range * (log(cnt_in_range / width)) +
+                prior(N, wh_in_edge[end], cnt_in_range)
+            if i > 1
+                fitness += best[i-1]
+            end
+            if fitness > fit_max
+                fit_max = fitness
+                i_max = i
+            end
+        end
+
+        # find the max of the fitness: this is the Q^th changepoint
+        lasts[Q] = i_max
+        best[Q] = fit_max
+    end
+
+    # Recover changepoints by iteratively peeling off the last block
+    change_points = Int[]
+    sizehint!(change_points, N)
+    ind = N + 1
+    while true
+        push!(change_points, ind)
+        ind <= 1 && break
+        ind = lasts[ind-1]
+    end
+    reverse!(change_points)
+    println("change_points = ", change_points)
+    edges = edges[change_points]
+    println("edges = ", edges)
+
+    # Evaluate densities and heights
+    return build_blocks(t, edges, weights)
+end
+
+export BHist, bayesian_blocks
+end
---
+++ Julia/test_bayes.jl
@@ -0,0 +1,34 @@
+const XRISM_RESOLVE_Pi2evFactor = 0.5f0
+
+# using a local debug version of BayesHistogram.jl
+include("BayesHistogram_debug.jl")
+
+using .BayesHistogram
+using DelimitedFiles
+using FITSIO
+
+Resolve = "file://" * homedir() * "/NAO/JAXA/XRISM/MAXI_J1744-294/xa901002010rsl_p0px5000_cl.evt.gz"
+println(Resolve)
+f = FITS(Resolve)
+
+for hdu in f
+    println(typeof(hdu))
+end
+
+# list column names in the second HDU
+println(FITSIO.colnames(f[2]))
+energy = Float32.(read(f[2], "PI")) .* XRISM_RESOLVE_Pi2evFactor
+
+
+nevents = length(energy)
+println("nevents = ", nevents)
+
+# take the energy between 0.5 and 20 keV
+energy = energy[(energy.>500.0).&(energy.<10000.0)]
+nevents = length(energy)
+println("nevents = ", nevents)
+
+@time bl = BayesHistogram.bayesian_blocks(energy)
+
+# export the energy to a text file
+writedlm("energy.txt", energy)
