e1acf09d0b1a05fb1bd0bc1b87f8d471a74e867a
jvo203 <chris.zapart@nao.ac.jp>
2025-03-31 12:52:41 +0900

    adjusted the Fortran code path

--- Julia/FORTRAN.jl
+++ Julia/FORTRAN.jl
@@ -1,7 +1,7 @@
 include("fortran_toolchain.jl")
 
 # a file with Fortran code
-code = "../Fortran/fbh.f90"
+code = "Fortran/fbh.f90"
 
 # compile the SPMD code
 lib = load_fortran(code, `-mcmodel=small -Ofast -fopenmp -ftree-vectorize -ftree-vectorizer-verbose=1 -funroll-loops -fmax-stack-var-size=32768`)
