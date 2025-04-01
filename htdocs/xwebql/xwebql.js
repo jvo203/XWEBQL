c89e1afde2ac736d502f4a052d8066a898542869
jvo203 <chris.zapart@nao.ac.jp>
2025-04-01 13:27:55 +0900

    removed the no-longer used code

--- htdocs/xwebql/xwebql.js
+++ htdocs/xwebql/xwebql.js
@@ -1,5 +1,5 @@
 function get_js_version() {
-    return "JS2025-03-31.0";
+    return "JS2025-04-01.0";
 }
 
 function uuidv4() {
@@ -5742,8 +5742,8 @@ function setup_image_selection() {
                             width: _width,
                             height: _height,
                             beam: zoom_shape,
-                            frame_start: Math.log(1000 * data_band_lo),// - 0.5 * fitsData.CDELT3,
-                            frame_end: Math.log(1000 * data_band_hi),// + 0.5 * fitsData.CDELT3,
+                            frame_start: Math.log(1000 * data_band_lo),
+                            frame_end: Math.log(1000 * data_band_hi),
                             seq_id: sent_seq_id,
                             timestamp: performance.now()
                         };
@@ -6592,8 +6592,8 @@ function imageTimeout() {
             width: _width,
             height: _height,
             beam: zoom_shape,
-            frame_start: Math.log(1000 * data_band_lo),// - 0.5 * fitsData.CDELT3,
-            frame_end: Math.log(1000 * data_band_hi),// + 0.5 * fitsData.CDELT3,
+            frame_start: Math.log(1000 * data_band_lo),
+            frame_end: Math.log(1000 * data_band_hi),
             seq_id: sent_seq_id,
             timestamp: performance.now()
         };
