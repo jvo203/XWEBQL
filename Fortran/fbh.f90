0f55354e071fff5900c83aa2d3fb956e4e609ff7
jvo203 <chris.zapart@nao.ac.jp>
2025-04-07 17:38:24 +0900

    experimenting with priors

--- Fortran/fbh.f90
+++ Fortran/fbh.f90
@@ -431,7 +431,7 @@ contains
             ! fitness = cnt_in_range * log(cnt_in_range / width) - log(wh_in_edge(size(wh_in_edge))) ! BIC
             ! fitness = cnt_in_range * log(cnt_in_range / width) - 2.0 ! AIC
             fitness = cnt_in_range * log(cnt_in_range / width) - 2.0 * log(log(wh_in_edge(size(wh_in_edge)))) ! HQIC
-            ! fitness = cnt_in_range * log(cnt_in_range / width) - log(0.2/0.8) ! Geometric(gamma)
+            ! fitness = cnt_in_range * log(cnt_in_range / width) - log(0.8/0.2) ! Geometric(gamma)
 
             if (i.gt. 1) fitness = fitness + best(i-1)
 
