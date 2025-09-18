b98d57e8a85cfd6b70190735466d3f71825a2857
Christopher Zapart <chris.zapart@nao.ac.jp>
2025-09-18 16:30:05 +0900

    SPMD --> Fortran

--- Julia/FORTRAN.jl
+++ Julia/FORTRAN.jl
@@ -3,7 +3,7 @@ include("fortran_toolchain.jl")
 # a file with Fortran code
 code = "Fortran/fbh.f90"
 
-# compile the SPMD code
+# compile the Fortran code
 lib = load_fortran(code, `-mcmodel=small -Ofast -fopenmp -ftree-vectorize -ftree-vectorizer-verbose=1 -funroll-loops -fmax-stack-var-size=32768`)
 
 # get function pointers
