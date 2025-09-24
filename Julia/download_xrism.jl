d8275b6cc77c7c226ecb5afceb63eac9ede8d38f
jvo203 <chris.zapart@nao.ac.jp>
2025-09-24 15:22:40 +0900

    extracting a table of href elements

--- Julia/download_xrism.jl
+++ Julia/download_xrism.jl
@@ -1,14 +1,34 @@
 using Downloads
 using Cascadia, Gumbo, HTTP
 
-#const pub = "https://data.darts.isas.jaxa.jp/pub/xrism/browse/public_list/"
 const pub = "https://data.darts.isas.jaxa.jp/pub/xrism/browse/public_list/?k=time&o=asc&c=ALL&q="
+const root = "https://data.darts.isas.jaxa.jp/pub/xrism/data/obs/rev3/"
 
-function get_root(pub)
-    html = HTTP.get(pub).body
-    doc = parsehtml(html)
+function get_table(pub)
+    req = HTTP.get(pub)
+    html = parsehtml(String(req.body))
+    println(html)
 
-    println(doc)
+    # get the table element (id = obs-list)
+    table = eachmatch(sel"#obs-list", html.root)
+    println(table)
 end
 
-get_root(pub)
+function get_root(root::String)
+    url = root
+    println(url)
+
+    # download the HTML, parse the table    
+    req = HTTP.get(url)
+    html = parsehtml(String(req.body))
+
+    # get all href elements
+    hrefs = eachmatch(sel"a", html.root)
+
+    # get all href attributes
+    hrefs = map(x -> x.attributes["href"], hrefs)
+    println(hrefs)
+end
+
+get_table(pub)
+get_root(root)
