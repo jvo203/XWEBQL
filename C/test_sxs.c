e9e6b99c877c6f5ea23e9b817bec21a28021193f
jvo203 <chris.zapart@nao.ac.jp>
2025-10-03 11:37:59 +0900

    adapted the C code to XRISM

--- C/test_sxs.c
+++ C/test_sxs.c
@@ -6,7 +6,9 @@
 
 int main()
 {
-    const char *event_filename = "../../../NAO/JAXA/ah100040060sxs_p0px1010_cl.evt";
+    // const char *event_filename = "../../../NAO/JAXA/ah100040060sxs_p0px1010_cl.evt"; // HITOMI
+    // const char *event_filename = "/tmp/xa300018010xtd_p0300000a0_cl.evt"; // XRISM
+    const char *event_filename = "../../../NAO/JAXA/XRISM/xa000129000xtd_p030000010_cl.evt"; // XRISM
     printf("event_filename = %s\n", event_filename);
 
     int16_t *x = NULL;
@@ -34,14 +36,14 @@ int main()
     // print the first and last values
     printf("x[0] = %d\n", x[0]);
     printf("y[0] = %d\n", y[0]);
-    printf("energy[0] = %f\n", energy[0]);
+    printf("energy[0] = %g\n", energy[0]);
 
     printf("x[%d] = %d\n", num_events - 1, x[num_events - 1]);
     printf("y[%d] = %d\n", num_events - 1, y[num_events - 1]);
-    printf("energy[%d] = %f\n", num_events - 1, energy[num_events - 1]);
+    printf("energy[%d] = %g\n", num_events - 1, energy[num_events - 1]);
 
     // sum x, y and energy
-    int64_t x_sum = 0;
+    /*int64_t x_sum = 0;
     int64_t y_sum = 0;
     double energy_sum = 0.0;
 
@@ -54,7 +56,7 @@ int main()
 
     printf("x_sum = %lld\n", x_sum);
     printf("y_sum = %lld\n", y_sum);
-    printf("energy_sum = %f\n", energy_sum);
+    printf("energy_sum = %g\n", energy_sum);*/
 
     free(x);
     free(y);
