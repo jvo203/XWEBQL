07d9fe2db85be2a04e1a729dbbba245380bd7c87
jvo203 <chris.zapart@nao.ac.jp>
2025-10-10 13:35:39 +0900

    parse the argument list

--- Julia/preload_xrism.jl
+++ Julia/preload_xrism.jl
@@ -1,18 +1,35 @@
 using HTTP
 using JSON
 
-mission = "xrism"
-host = "jvow" # jvow or grid82
-port = 10000
-dir = "/Volumes/OWC/JAXA/XRISM"
+# default values
+mission = "xrism" # --mission
+host = "jvow" # jvow or grid82 # --host
+port = 10000 # --port
+dir = "/Volumes/OWC/JAXA/XRISM" # --dir
 
-# get the dir from the first command line argument
-if length(ARGS) > 0
-    dir = ARGS[1]
-    #else
-    #    println("Usage: julia preload_xrism.jl <xrism_dir>")
-    #    exit(1)
+if length(ARGS) == 0
+    println("Usage: julia preload_xrism.jl --mission=<mission> --host=<host> --port=<port> --dir=<xrism_dir>")
+    println("e.g.: julia preload_xrism.jl --mission=xrism --host=jvow --port=10000 --dir=/Volumes/OWC/JAXA/XRISM")
+    exit(1)
 end
+
+# get the arguments from the command line
+for arg in ARGS
+    if startswith(arg, "--mission=")
+        global mission = split(arg, "=")[2]
+    elseif startswith(arg, "--host=")
+        global host = split(arg, "=")[2]
+    elseif startswith(arg, "--port=")
+        global port = parse(Int, split(arg, "=")[2])
+    elseif startswith(arg, "--dir=")
+        global dir = split(arg, "=")[2]
+    end
+end
+
+println("mission: ", mission)
+println("host: ", host)
+println("port: ", port)
+println("dir: ", dir)
 
 function get_dataset_url(dataset)
     # HTTP-encode the dataset    
@@ -25,7 +42,6 @@ function get_dataset_url(dataset)
            "&dataset=" *
            HTTP.escapeuri(dataset)
 end
-
 
 function poll_progress(dataset)
     strURL =
@@ -98,7 +114,7 @@ end
 function process_xrism_directory(dir)
     println("process_xrism_directory dir: ", dir)
 
-    entries = readdir(dir; join = false)
+    entries = readdir(dir; join=false)
     println("total number of entries: ", length(entries))
 
     for dataset in entries
@@ -115,5 +131,4 @@ function process_xrism_directory(dir)
 
 end
 
-println("preload_xrism.jl dir: ", dir)
 process_xrism_directory(dir)
