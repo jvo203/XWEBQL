6564d3594f5960fe370b012487e4bc54bcc947bc
jvo203 <chris.zapart@nao.ac.jp>
2026-05-22 10:45:27 +0900

    switch over to a local surface.js

--- htdocs/xwebql/xwebql.js
+++ htdocs/xwebql/xwebql.js
@@ -1,5 +1,5 @@
 function get_js_version() {
-    return "JS2026-03-18.0";
+    return "JS2026-05-22.0";
 }
 
 function uuidv4() {
@@ -388,8 +388,8 @@ function enable_3d_view() {
 
         (function () {
             var po = document.createElement('script'); po.type = 'text/javascript'; po.async = false;
-            // po.src = 'surface.js' + '?' + encodeURIComponent(get_js_version());            
-            po.src = 'https://cdn.jsdelivr.net/gh/jvo203/XWEBQL@' + htmlData.getAttribute('data-version-major') + '.' + htmlData.getAttribute('data-version-minor') + '.' + htmlData.getAttribute('data-version-sub') + '/htdocs/xwebql/surface.min.js';
+            po.src = 'surface.js' + '?' + encodeURIComponent(get_js_version());
+            //po.src = 'https://cdn.jsdelivr.net/gh/jvo203/XWEBQL@' + htmlData.getAttribute('data-version-major') + '.' + htmlData.getAttribute('data-version-minor') + '.' + htmlData.getAttribute('data-version-sub') + '/htdocs/xwebql/surface.min.js';
             var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
         })();
 
